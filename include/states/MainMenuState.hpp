b2d2cacc2d55151e1ba1fb8de0e2846ca2a2ae1e
Siqek <113335428+Siqek@users.noreply.github.com>
2025-05-24 18:28:07 +0200

    Add rectangle representing start button in MainMenuState

--- include/states/MainMenuState.hpp
+++ include/states/MainMenuState.hpp
@@ -11,11 +11,20 @@ class MainMenuState
 public:
     MainMenuState(sf::RenderWindow* window, const std::unordered_map<std::string, int>& supportedKeys, const sf::Font& font);
 
+    void onWindowResize() override;
+
     void updateInput() override;
     void update(const float& dt) override;
+
     void render(sf::RenderTarget* target = nullptr) override;
+
 private:
     sf::RectangleShape rs;
+
+    sf::RectangleShape startButton;
+    sf::Text startButtonText;
+
+    void updateUIScaling();
 };
 
 #endif // MAINMENUSTATE_HPP
--- src/states/MainMenuState.cpp
+++ src/states/MainMenuState.cpp
@@ -6,11 +6,31 @@ MainMenuState::MainMenuState(sf::RenderWindow* window, const std::unordered_map<
 {
     rs.setFillColor(sf::Color::Red);
     rs.setSize(sf::Vector2f(20.f, 20.f));
+
+    this->startButtonText.setString("Play");
+    this->startButtonText.setFont(font);
+    this->startButtonText.setFillColor(sf::Color(0x1C1427FF));
+
+    this->startButton.setFillColor(sf::Color(0x40394AFF));
+    this->startButton.setOutlineColor(sf::Color(0x1C1427FF));
+}
+
+void MainMenuState::onWindowResize()
+{
+    this->updateUIScaling();
 }
 
 void MainMenuState::updateInput()
 {
-
+    if (sf::Mouse::isButtonPressed(sf::Mouse::Button::Right))
+    {
+        if (this->startButton.getGlobalBounds().contains(sf::Vector2f(
+            static_cast<float>(this->mouseScreenPos.x),
+            static_cast<float>(this->mouseScreenPos.y))))
+        {
+            // push GameState to state stack (Game::states)
+        }
+    }
 }
 
 void MainMenuState::update(const float& dt)
@@ -28,5 +48,30 @@ void MainMenuState::render(sf::RenderTarget* target)
     if (target == nullptr)
         target = this->window;
 
+    target->draw(this->startButton);
+    target->draw(this->startButtonText);
+
     target->draw(this->rs);
+}
+
+void MainMenuState::updateUIScaling()
+{
+    sf::Vector2f windowSize(
+        static_cast<float>(this->window->getSize().x),
+        static_cast<float>(this->window->getSize().y)
+    );
+
+    this->startButtonText.setPosition(windowSize / 2.f);
+    this->startButtonText.setCharacterSize(static_cast<unsigned>(std::min(windowSize.x * 0.04f, windowSize.y * 0.04f)));
+
+    auto textlb = this->startButtonText.getLocalBounds();
+    this->startButtonText.setOrigin(sf::Vector2f(textlb.left + textlb.width / 2.f, textlb.top + textlb.height / 2.f));
+
+    this->startButton.setPosition(this->startButtonText.getPosition());
+    this->startButton.setSize(sf::Vector2f(textlb.width * 4.f, textlb.height * 1.8f));
+
+    this->startButton.setOutlineThickness(this->startButton.getSize().y * 0.05f);
+
+    auto buttonlb = this->startButton.getLocalBounds();
+    this->startButton.setOrigin(sf::Vector2f(buttonlb.left + buttonlb.width / 2.f, buttonlb.top + buttonlb.height / 2.f));
 }
