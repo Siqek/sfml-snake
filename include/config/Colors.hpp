c37c29fdb8d6d04d0719103fce1e1599ea1e6b31
Siqek <113335428+Siqek@users.noreply.github.com>
2025-05-31 18:46:55 +0200

    Add UI color constants

---
+++ include/config/Colors.hpp
@@ -0,0 +1,32 @@
+#ifndef COLORS_HPP
+#define COLORS_HPP
+
+namespace Colors::Hex {
+    static constexpr std::uint32_t Background          = 0x0F0E17FF;
+
+    static constexpr std::uint32_t TextMain            = 0xE9FFE0FF;
+    static constexpr std::uint32_t TextMainOutline     = 0x0B0B10FF;
+
+    static constexpr std::uint32_t TextAlt             = 0xF5F5FAFF;
+    static constexpr std::uint32_t TextAltOutline      = 0x2B2633FF;
+
+
+    static constexpr std::uint32_t SnakeBody           = 0xCCFFBDFF;
+    static constexpr std::uint32_t SnakeOutline        = 0x7ECA9CFF;
+
+
+    static constexpr std::uint32_t BoardCellPrimary    = 0x40394AFF;
+    static constexpr std::uint32_t BoardCellSecondary  = 0x1C1427FF;
+
+
+    static constexpr std::uint32_t ButtonIdleBg        = 0xFFFFFFFF;
+    static constexpr std::uint32_t ButtonIdleOutline   = 0xFFFFFFFF;
+
+    static constexpr std::uint32_t ButtonHoverBg       = 0xFFFFFFFF;
+    static constexpr std::uint32_t ButtonHoverOutline  = 0xFFFFFFFF;
+
+    static constexpr std::uint32_t ButtonActiveBg      = 0xFFFFFFFF;
+    static constexpr std::uint32_t ButtonActiveOutline = 0xFFFFFFFF;
+}
+
+#endif // COLORS_HPP
