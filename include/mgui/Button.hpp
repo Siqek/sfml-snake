bb75e75f80a9618409bfc0766a0c80d5061436c0
Siqek <113335428+Siqek@users.noreply.github.com>
2025-06-03 19:17:30 +0200

    Add Button class

---
+++ include/mgui/Button.hpp
@@ -0,0 +1,79 @@
+#ifndef MGUI_BUTTON_HPP
+#define MGUI_BUTTON_HPP
+
+namespace mgui
+{
+    enum class ButtonState {
+        Idle,
+        Hover,
+        Active
+    };
+
+    class Button {
+    public:
+        Button();
+        ~Button() = default;
+
+        void setSize(const sf::Vector2f& size);
+        void setPosition(const sf::Vector2f& position);
+        void setOrigin(const sf::Vector2f& origin);
+
+        void setText(const std::string& text);
+        void setFont(const sf::Font& font) { label.setFont(font); };
+        void setCharacterSize(unsigned size);
+
+        void setFillColor(ButtonState state, const sf::Color& color);
+        void setOutlineThickness(float thickness) { shape.setOutlineThickness(thickness); };
+
+        ////////////////////////////////////////////////////////////
+        /// \brief Sets the accent color (text and outline)
+        ///        for the specified button state.
+        ///
+        /// This function assigns a common color to both the
+        /// button's text and its outline depending on its state.
+        ///
+        /// \param state The button state for which the accent
+        ///              color should be applied.
+        /// \param color The color to be used for the text and
+        ///              outline in the given state.
+        ///
+        ////////////////////////////////////////////////////////////
+        void setAccentColor(ButtonState state, const sf::Color& color);
+
+        const sf::Vector2f& getSize() const { return shape.getSize(); };
+
+        bool isPressed() const { return state == ButtonState::Active; };
+        bool isReleased() const { return prevState == ButtonState::Active && state == ButtonState::Hover; };
+
+        void update(const sf::RenderWindow& window);
+        void render(sf::RenderTarget& target);
+
+    private:
+        struct ColorScheme {
+            sf::Color fillColor;
+            sf::Color accentColor;
+
+            ColorScheme()
+                : fillColor(0x000000FF), accentColor(0xFFFFFFFF) {}
+        };
+
+        sf::RectangleShape shape;
+        sf::Text label;
+
+        ColorScheme idleColor;
+        ColorScheme hoverColor;
+        ColorScheme activeColor;
+
+        ButtonState prevState;
+        ButtonState state;
+
+        void updateState(const sf::RenderWindow& window);
+
+        void updateTextOrigin();
+
+        void updateColor();
+        void applyColorScheme(const ColorScheme& colorScheme);
+    };
+}
+
+#endif // MGUI_BUTTON_HPP
---
+++ src/mgui/Button.cpp
@@ -0,0 +1,143 @@
+#include "stdafx.hpp"
+#include "mgui/Button.hpp"
+
+namespace mgui
+{
+    Button::Button()
+        : prevState(ButtonState::Idle), state(ButtonState::Idle)
+    {
+        this->applyColorScheme(this->idleColor);
+    }
+
+    void Button::setSize(const sf::Vector2f& size)
+    {
+        this->shape.setSize(size);
+        this->updateTextOrigin();
+    }
+
+    void Button::setPosition(const sf::Vector2f& position)
+    {
+        this->shape.setPosition(position);
+        this->label.setPosition(position);
+    }
+
+    void Button::setOrigin(const sf::Vector2f& origin)
+    {
+        this->shape.setOrigin(origin);
+        this->updateTextOrigin();
+    }
+
+    void Button::setText(const std::string& text)
+    {
+        this->label.setString(text);
+        this->updateTextOrigin();
+    }
+
+    void Button::setCharacterSize(unsigned size)
+    {
+        this->label.setCharacterSize(size);
+        this->updateTextOrigin();
+    }
+
+    void Button::setFillColor(ButtonState state, const sf::Color& color)
+    {
+        switch (state)
+        {
+        case ButtonState::Idle:
+            this->idleColor.fillColor = color;
+            break;
+        case ButtonState::Hover:
+            this->hoverColor.fillColor = color;
+            break;
+        case ButtonState::Active:
+            this->activeColor.fillColor = color;
+            break;
+        default:
+            break;
+        }
+    }
+
+    void Button::setAccentColor(ButtonState state, const sf::Color& color)
+    {
+        switch (state)
+        {
+        case ButtonState::Idle:
+            this->idleColor.accentColor = color;
+            break;
+        case ButtonState::Hover:
+            this->hoverColor.accentColor = color;
+            break;
+        case ButtonState::Active:
+            this->activeColor.accentColor = color;
+            break;
+        default:
+            break;
+        }
+    }
+
+    void Button::update(const sf::RenderWindow& window)
+    {
+        this->updateState(window);
+    }
+
+    void Button::render(sf::RenderTarget& target)
+    {
+        target.draw(this->shape);
+        target.draw(this->label);
+    }
+
+    void Button::updateState(const sf::RenderWindow& window)
+    {
+        this->prevState = this->state;
+
+        if (this->shape.getGlobalBounds().contains(sf::Vector2f(sf::Mouse::getPosition(window))))
+        {
+            if (sf::Mouse::isButtonPressed(sf::Mouse::Button::Left)) {
+                this->state = ButtonState::Active;
+            } else {
+                this->state = ButtonState::Hover;
+            }
+        } else {
+            this->state = ButtonState::Idle;
+        }
+
+        if (this->prevState != this->state)
+            this->updateColor();
+    }
+
+    void Button::updateTextOrigin()
+    {
+        const auto& shapeSize = this->shape.getSize();
+        const auto& shapeOrigin = this->shape.getOrigin();
+        const auto lb = this->label.getLocalBounds();
+        this->label.setOrigin(sf::Vector2f(
+            lb.left + lb.width / 2.f - shapeSize.x / 2.f + shapeOrigin.x,
+            lb.top + lb.height / 2.f - shapeSize.y / 2.f + shapeOrigin.y
+        ));
+    }
+
+    void Button::updateColor()
+    {
+        switch (this->state)
+        {
+        case ButtonState::Idle:
+            this->applyColorScheme(this->idleColor);
+            break;
+        case ButtonState::Hover:
+            this->applyColorScheme(this->hoverColor);
+            break;
+        case ButtonState::Active:
+            this->applyColorScheme(this->activeColor);
+            break;
+        default:
+            break;
+        }
+    }
+
+    void Button::applyColorScheme(const ColorScheme& colorScheme)
+    {
+        this->shape.setFillColor(colorScheme.fillColor);
+        this->shape.setOutlineColor(colorScheme.accentColor);
+        this->label.setFillColor(colorScheme.accentColor);
+    }
+}
